38ed9b52c2224c509cf8b9269e74206f4957670d
Victor <victor.baldin.2005@gmail.com>
2024-03-25 23:15:35 +0300

    complete separation into the library

--- sfml_wrapper.h
+++ sfml_wrapper.h
@@ -8,4 +8,101 @@
 #ifndef SFML_WRAPPER_H_
 #define SFML_WRAPPER_H_
 
+#include <assert.h>
+
+#include <SFML/Graphics.hpp>
+#include <fontconfig/fontconfig.h>
+
+struct SfmlGui {
+    sf::RenderWindow window;
+    sf::Texture texture;
+    sf::Sprite sprite;
+    sf::Text text;
+    sf::Font font;
+        
+    uint8_t* colors;
+    
+    unsigned width;
+    unsigned height;
+    
+    ~SfmlGui() {};
+};
+
+struct RgbaPixel {
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+    uint8_t alpha;
+};
+
+struct SfmlGuiText {
+    const char* font_name;
+    sf::Color color;
+    unsigned font_size;
+};
+
+inline bool sfml_gui_set_font(SfmlGui* gui, const char* name)
+{
+    bool is_set = true;
+ 
+    FcConfig* config = FcInitLoadConfigAndFonts();
+    FcPattern* pat = FcNameParse((const FcChar8*)name);
+    FcConfigSubstitute(config, pat, FcMatchPattern);
+    FcDefaultSubstitute(pat);
+    
+    FcResult res;
+    FcPattern* font = FcFontMatch(config, pat, &res);
+    if (font != nullptr) {
+        FcChar8* file = nullptr;
+        if (FcPatternGetString(font, FC_FILE, 0, &file) == FcResultMatch) {
+            if (!gui->font.loadFromFile((char*)file)) {
+                is_set = false;
+                goto cleanup;
+            }
+        }
+    }
+    
+    gui->text.setFont(gui->font);
+
+cleanup:
+    FcPatternDestroy(pat);
+    FcPatternDestroy(font);    
+    return is_set;
+} 
+
+inline bool sfml_gui_create(SfmlGui* gui, const char* title, 
+                            unsigned width, unsigned height,
+                            SfmlGuiText text)
+{
+    assert(gui != nullptr);
+    
+    gui->window.create(sf::VideoMode(width, height), title);
+    gui->texture.create(width, height);
+    gui->sprite.setTexture(gui->texture);
+    
+    gui->colors = (uint8_t*)calloc(width * height * 4, sizeof(*gui->colors));
+    if (gui->colors == nullptr) {
+        fprintf(stderr, "ERROR: Could not create GUI: allocation failure\n");
+        return false;
+    }
+    
+    gui->width = width;
+    gui->height = height;
+    gui->text.setCharacterSize(text.font_size);
+    gui->text.setFillColor(text.color);
+    
+    if (!sfml_gui_set_font(gui, text.font_name)) {
+        fprintf(stderr, "WARNING: could not load font\n");
+        return false;
+    }
+
+    return true;
+}
+
+inline void sfml_gui_destroy(SfmlGui* plot)
+{
+    free(plot->colors);
+}
+
+
 #endif // SFML_WRAPPER_H_
